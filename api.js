301b35a5634e2e272cb525e203cd0b2bf91dfe47
Diego Martinez <diegofernandomartinezcalderon@gmail.com>
2023-05-20 09:13:13 -0500

    feat: :lipstick: incorporación de links
    en este se incorporan los principales links de la busqueda

--- api.js
+++ api.js
@@ -1,24 +1,23 @@
-async function buscarGoogle(){
-const buscar = document.getElementById("barraBuscar").value;
-const url = `https://google-search74.p.rapidapi.com/?query=${buscar}&limit=10&related_keywords=true`;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '282e8ded6amsh389b2ead7fe7657p11c32cjsn6b13b7ade005',
-		'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
-	}
-};
+//keys=
+//adaad128aemsh6cfb78feb6e838dp15a6d2jsn85a17c09c579
+//4d3abdd95amsh42b11a056be5dccp19703cjsn96a51d54275a
+//3c3a7a1fa4msh0e92c078fd61424p137867jsnebb20098d172
+async function buscarGoogle() {
+	const buscar = document.getElementById("barraBuscar").value;
+	const url = `https://google-search74.p.rapidapi.com/?query=${buscar}&limit=10&related_keywords=true`;
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '282e8ded6amsh389b2ead7fe7657p11c32cjsn6b13b7ade005',
+			'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+		}
+	};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-    const wiki=result.results[0];
-    console.log(wiki)
-    const description=result.results[0].description;
-	const title=result.results[0].url;
-    console.log(description)
-    document.getElementById("info").innerHTML = `
+	try {
+		const response = await fetch(url, options);
+		const result = await response.json();
+		console.log(result);
+		document.getElementById("info").innerHTML = `
 	<div class="container" >
 	<div class="row">
 		<div class="col-lg-2" style="padding-right: 0px; ">
@@ -40,15 +39,72 @@ try {
 			</button>
 		</div>
 	</div>
+       <br/>
+	   <br/>
 
 	<div class="row">
-	    <h1>${title}</h1>
-		<h3>${description}</h3>
+	<a href="${result.results[0].url}"style="text-decoration: none;"><h3>${result.results[0].title}</h3></a>
+		<h6>${result.results[0].description}</h6>
 	</div>
+    <br/>
+	<div class="row">
+	<a href="${result.results[1].url}"style="text-decoration: none;"><h3>${result.results[1].title}</h3></a>
+		<h6>${result.results[1].description}</h6>
+	</div>
+	<br/>
+	<div class="row">
+	<a href="${result.results[2].url}"style="text-decoration: none;"><h3>${result.results[2].title}</h3></a>
+		<h6>${result.results[2].description}</h6>
+	</div>
+	<br/>
+	<div class="row">
+	<a href="${result.results[3].url}"style="text-decoration: none;"><h3>${result.results[3].title}</h3></a>
+		<h6>${result.results[3].description}</h6>
+	</div>
+	<br/>
+	<div class="row">
+	<a href="${result.results[4].url}"style="text-decoration: none;"><h3>${result.results[4].title}</h3></a>
+		<h6>${result.results[4].description}</h6>
+	</div>
+	<br/>
+	<div class="row">
+	<a href="${result.results[5].url}"style="text-decoration: none;"><h3>${result.results[5].title}</h3></a>
+		<h6>${result.results[5].description}</h6>
+	</div>
+	<br/>
+	<a href="${result.results[6].url}"style="text-decoration: none;"><h3>${result.results[6].title}</h3></a>
+		<h6>${result.results[6].description}</h6>
+	</div>
+	<br/>
+
+	
+
 </div> 
 `;
 
-} catch (error) {
-	console.error(error);
+	} catch (error) {
+		console.error(error);
+	}
 }
- }
+
+
+
+
+
+
+//const url = 'https://google-search72.p.rapidapi.com/search?query=word%20cup&gl=us&lr=en&num=10&start=0&sort=relevance';
+//const options = {
+//	method: 'GET',
+//	headers: {
+//		'X-RapidAPI-Key': 'adaad128aemsh6cfb78feb6e838dp15a6d2jsn85a17c09c579',
+//		'X-RapidAPI-Host': 'google-search72.p.rapidapi.com'
+//	}
+//};
+//
+//try {
+//	const response = await fetch(url, options);
+//	const result = await response.json();
+//	console.log(result);
+//} catch (error) {
+//	console.error(error);
+//}
